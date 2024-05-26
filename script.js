3d71385ecd75ecd8dcb701014589913d349202cf
Maike <maikepereirapro@gmail.com>
2024-05-27 01:54:02 +0200

    portfolio

--- script.js
+++
@@ -1,4 +0,0 @@
-// Fonction pour basculer l'affichage du menu déroulant
-document.querySelector('.menu-toggle').addEventListener('click', function() {
-    document.querySelector('nav ul').classList.toggle('show');
-});
