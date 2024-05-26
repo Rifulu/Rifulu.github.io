2d2ebab1dedda1d246f29a11b5023810484f8102
Maike <maikepereirapro@gmail.com>
2024-05-26 21:29:12 +0200

    initial comit

---
+++ script.js
@@ -0,0 +1,4 @@
+// Fonction pour basculer l'affichage du menu déroulant
+document.querySelector('.menu-toggle').addEventListener('click', function() {
+    document.querySelector('nav ul').classList.toggle('show');
+});
